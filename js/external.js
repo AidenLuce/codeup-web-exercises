19d66ee05acabe35f7f07f6f6f5482cf3fd64131
Aiden Luce <luceaiden4124@gmail.com>
2022-08-12 14:28:14 -0500

    exercises

---
+++ js/external.js
@@ -0,0 +1,26 @@
+// console.log("Hello from external JavaScript")
+// alert("Welcome to my Website!")
+// let favoriteColor = prompt("What is your favorite color?");
+// alert(favoriteColor + " is my favorite color too!");
+
+// You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear (for 5 days, they love it), and Hercules (1 day, you don't know yet if they're going to like it). If price for a movie per day is $3, how much will you have to pay?
+
+//     let daysWithTheLittleMermaid = prompt("How long did you rent The Little Mermaid")
+//     let daysWithBrotherBear = prompt("How long did you rent Brother Bear? ")
+//     let daysWithHercules = prompt("How long did you rent Hercules")
+//         alert(daysWithTheLittleMermaid * 3 + daysWithBrotherBear * 3 + daysWithHercules * 3)
+
+//     Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. Google pays $400, Amazon $380, and Facebook $350. How much will you receive in payment for this week? You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
+    let googleHours = prompt("How many hours did you work at Google?")
+    let googlePay = prompt("What was you pay at Google?" )
+    let amazonHours = prompt("How many hours did you work at Amazon?")
+    let amazonPay = prompt("What was you pay at Amazon?")
+    let facebookHours = prompt("How many hours did you work at Facebook?")
+    let facebookPay= prompt("What was you pay at FaceBook?")
+        alert("Your total pay for this week is $" + (Number(googleHours * googlePay) + Number(amazonHours * amazonPay) + Number(facebookHours * facebookPay)) )
+
+// A student can be enrolled in a class only if the class is not full and the class schedule does not conflict with her current schedule.
+    let classNotFull = prompt("Is there space available in the class")
+    let noScheduleConflict = prompt("Do you have time in your schedule for this class?")
+
+//     A product offer can be applied only if a person buys more than 2 items, and the offer has not expired. Premium members do not need to buy a specific amount of products.
---
+++ js/functions-lecture.js
@@ -0,0 +1,69 @@
+/* The parts of a function */
+//1. function keyword
+// 2. name of the function
+// 3. parentheses with zero or mor parameters
+// 4. If there is more than one parameter, the list is comma seperated
+// 5. Curly braces contain the function body
+// 6. The function body contains the code that runs when the function is invoked
+// 7. [optional but not common] return statement
+
+// function sayHello(greeting, name){
+//     return `${greeting}, ${name}`;
+// }
+
+// Here number has local scope
+
+// function localOrGlobal() {
+//     let number =20;
+//     console.log(number);
+// }
+
+// Here number has global scope
+
+// let number =20;
+// function localOrGlobal(){
+//     console.log(number);
+// }
+
+let number = 20;
+
+//function decloration
+
+// function localOrGlobal(){
+//     let number = 10;
+//     console.log(number);
+// }
+// // localOrGlobal();
+// // console.log(number);
+//
+// //function expression
+//
+//
+//
+// const bark = function(){
+//     console.log("WOOOF!");
+// }
+//
+// function yap(){
+//     console.log("yip yip");
+// }
+
+
+
+
+//function declaration
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+//function expression
+const add = function(num1, num2){
+    return num1 + num2;
+}
+
+//arrow function
+const add = (num1, num2) => num1 + num2;
+
+
+
+
