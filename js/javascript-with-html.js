befd470b02b92c47f7929f3a000e9889ab98f4ef
Aiden Luce <luceaiden4124@gmail.com>
2022-08-12 11:15:15 -0500

    exercises

---
+++ js/javascript-with-html.js
@@ -0,0 +1,8 @@
+// let userChoice = confirm("sfdgfhgjhkjhjghfgd");
+// if(userChoice){
+//     alert("thank you come again")
+// } else{
+//     alert(":(")
+// }
+
+let name = prompt("What's your name?")
