3704a287589455a93f20ac9ef11a1daf0e38f9a6
Aiden Luce <luceaiden4124@gmail.com>
2022-10-03 15:44:59 -0500

    exercises completed

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -36,6 +36,36 @@ const users = [
     }
 ];
 
-
-
+// Q1
+let overThree = users.filter(function(e){
+    return e.languages.length >= 3;
+})
 console.log(overThree)
+
+// Q2
+let emails = users.map(function(e){
+    return e.email
+});
+console.log(emails)
+
+// Q3
+
+let totalYears = users.reduce((accumulator, years)  => {
+    return accumulator + years.yearsOfExperience
+}, 0)
+console.log(totalYears/users.length)
+
+// Q4
+const longestEmail = users.reduce((accumulator, email) =>{
+    return accumulator.length > email.length ? accumulator : email.email    /**========================== HELP ============================*/
+    },);
+console.log(longestEmail)
+
+// Q5
+let user = users.reduce((pre, user, index, usersArray) =>{
+    return pre + `${pre ? ', ' : 'Your instructors: '}${user.name}${(index === (usersArray.length -1))? '.' :''}`
+},'');
+console.log(user)
+
+
+
